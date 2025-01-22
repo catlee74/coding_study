d23c9dc03a71151668d109291c36e9f2621206a6
catlee74 <162817725+catlee74@users.noreply.github.com>
2025-01-22 14:11:41 +0900

    const and window.prompt

---
+++ webcoding/class3/script.js
@@ -0,0 +1,16 @@
+// //변수
+// let aaa;
+// aaa = 5;
+// console.log(aaa);
+// aaa = 8;
+// console.log(aaa);
+
+// //상수
+// const bb = 5;
+// console.log(bb);
+
+// prompt('프롬프트 다이얼로그 샘플');
+
+const promptMessage = '프롬프트 다이얼로그 샘플';
+const result = prompt(promptMessage);
+console.log(result);
