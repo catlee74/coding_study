3f523877b99ce574027f85df148efa29611475b2
catlee74 <162817725+catlee74@users.noreply.github.com>
2025-02-14 12:07:44 +0900

    자바 연산과 연산자

---
+++ webcoding/class4/script.js
@@ -0,0 +1,27 @@
+//연산과 연산자
+
+//산술연산
+console.log(3 + 5);
+console.log(3 - 5);
+console.log(3 * 5);
+console.log(3 / 5);
+console.log(3 % 5);
+
+//대입연산
+let result = 1 + 1;
+console.log(result);
+
+result = 1 - 1;
+console.log(result);
+
+result = 1 * 1;
+console.log(result);
+
+result = 1 / 2;
+console.log(result);
+
+//복합 대입 연산자
+let number = 3;
+// number = number + 3;
+number += 3;
+console.log(number);
