30fb7e015c127659d47d6d4f4703d73cc242e17b
catlee74 <162817725+catlee74@users.noreply.github.com>
2025-02-19 14:15:17 +0900

    노트북 데이터 옮김

---
+++ webcoding/class5/script.js
@@ -0,0 +1,14 @@
+// let data;
+// console.log(data);
+
+// data = 'hello';
+// console.log(data);
+
+// data = null;
+// console.log(data);
+
+let data = true;
+console.log(typeof data);
+
+data = false;
+console.log(data);
