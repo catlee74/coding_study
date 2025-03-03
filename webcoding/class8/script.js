7ade555409ded9834d599aee39b27a2dba243021
catlee74 <162817725+catlee74@users.noreply.github.com>
2025-03-03 14:57:41 +0900

    자바_비교연산자

---
+++ webcoding/class8/script.js
@@ -0,0 +1,22 @@
+//비교연산
+let num1 = 2;
+let num2 = 5;
+
+//false
+console.log(num1 > num2);
+// true
+console.log(num1 < num2);
+
+//비교연산
+let num3 = '3';
+let num4 = 3;
+
+//true
+console.log(num3 == num4);
+//false
+console.log(num3 === num4);
+
+//false
+console.log(num3 != num4);
+//true
+console.log(num3 !== num4);
