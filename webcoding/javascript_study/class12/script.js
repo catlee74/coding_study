aaec955d85cb94c179c3068c3da67cff6912ea06
catlee74 <162817725+catlee74@users.noreply.github.com>
2025-03-27 00:22:20 +0900

    js return study

---
+++ webcoding/javascript_study/class12/script.js
@@ -0,0 +1,10 @@
+// const result = confirm('check!');
+// console.log(result);
+
+function Return() {
+  console.log('nothing');
+  return 10;
+}
+const result = Return();
+console.log(result);
+//변수는 있지만 데이터가 없는 상태
